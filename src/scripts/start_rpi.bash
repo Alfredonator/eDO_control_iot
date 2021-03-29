590780dfa38405a434665c3118595ce9e5bcf95d
Szymon <szymonso1009@gmail.com>
2021-03-29 19:11:42 +0200

    added startup script for roslanuch. No sourcing, etc. necessary

---
+++ src/scripts/start_rpi.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+master_ip="10.42.0.49"
+hostname=`hostname`
+
+export ROS_MASTER_URI=http://${master_ip}:11311
+export ROS_HOSTNAME=${hostname}.local
+
+#source ~/edo_ws/devel/setup.bash
+source ~/catkin_ws/devel/setup.bash
+roslaunch calib calibrate.launch
